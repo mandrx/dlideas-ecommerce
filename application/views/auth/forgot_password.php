62d18e7a6a3bba792508a46007128adca19d4b55
mandrx <mandrx@gmail.com>
2026-06-23 09:58:15 +0800

    fix: add set_value() to forgot_password email for UX consistency

--- application/views/auth/forgot_password.php
+++ application/views/auth/forgot_password.php
@@ -2,7 +2,7 @@
 <?= form_open('forgot-password/post') ?>
 <div class="mb-3">
     <label for="email" class="form-label">Email</label>
-    <input type="email" class="form-control" id="email" name="email" required>
+    <input type="email" class="form-control" id="email" name="email" value="<?= set_value('email') ?>" required>
 </div>
 <button type="submit" class="btn btn-primary w-100">Send Reset Link</button>
 <?= form_close() ?>
