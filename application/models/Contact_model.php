e958bc10598a8081f7fa6870bb7b16bed11a7742
mandrx <mandrx@gmail.com>
2026-06-24 16:53:56 +0800

    feat: add Contact_model

---
+++ application/models/Contact_model.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contact_model extends MY_Model
+{
+    protected $table   = 'contact_messages';
+    protected $primary = 'id';
+
+    public function save(array $data)
+    {
+        return $this->db->insert($this->table, $data);
+    }
+}
